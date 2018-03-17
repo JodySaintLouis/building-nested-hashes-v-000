10335faea259035898a0166126779e1f226a0a53
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-17 05:26:08 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -8,7 +8,7 @@ epic_tragedy = {
         :hero_friends => [
           {name: "Benvolio", age: "17", attitude: "worried"}
           {name: "Mercutio", age: "18"}, attitude: "hot-headed"
-        ]
+                          ]
                   },
 
      :capulet => {
