64eafa5bf357d1b2352bbd8a5ce3c783e79cdbcc
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-17 05:27:56 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -8,6 +8,7 @@ epic_tragedy = {
         :hero_friends => [
           {name: "Benvolio", age: "17", attitude: "worried"}
           {name: "Mercutio", age: "18"}, attitude: "hot-headed"
+
                           ]
                   },
 
@@ -16,7 +17,9 @@ epic_tragedy = {
        :patriarch => {name: "Lord Capulet", age: "50"}
        :matriarch => {name: "Lady Capulet", age: "51"}
        :heroine => {name: "Juliet", age: "15", status: "alive"}
-       :heroine_friends => []
+       :heroine_friends => [
+
+                          ]
 
                 }
   }
