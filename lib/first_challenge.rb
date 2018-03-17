09b244035ee345643f06f984b75237ebe384acb6
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-17 05:25:42 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -9,7 +9,6 @@ epic_tragedy = {
           {name: "Benvolio", age: "17", attitude: "worried"}
           {name: "Mercutio", age: "18"}, attitude: "hot-headed"
         ]
-
                   },
 
      :capulet => {
